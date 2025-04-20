2577596515fb6195c13f6f5ad823282f8e3f220b
tregueuxclubcanin <mikmakmouklfl+club@gmail.com>
2025-04-20 12:30:33 +0200

    Add files via upload

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import TregueuxSportCanin from "./TregueuxSportCanin";
+import "./style.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <TregueuxSportCanin />
+  </React.StrictMode>
+);
