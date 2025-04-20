f966be8196118e7329b1e247c43e34f86422f7e4
tregueuxclubcanin <mikmakmouklfl+club@gmail.com>
2025-04-20 14:09:30 +0200

    Update index.js

--- assets/index.js
+++ assets/index.js
@@ -1,8 +1,2 @@
+document.getElementById("root").innerHTML = "<h1>Bienvenue au Tregueux Sport Canin !</h1>";
 
-import React from "react";
-import ReactDOM from "react-dom/client";
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  React.createElement("h1", null, "Bienvenue au Tregueux Sport Canin !")
-);
